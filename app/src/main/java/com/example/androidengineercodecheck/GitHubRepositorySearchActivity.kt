f5f4489284d56f93789e4198623988ccb01ff879
shusuke0812 <shu.ota0812@gmail.com>
2020-12-05 16:52:27 +0900

    [rename]トップ画面のactivityファイル名とxmlファイル名を変更

--- app/src/main/java/com/example/androidengineercodecheck/GitHubRepositorySearchActivity.kt
+++ app/src/main/java/com/example/androidengineercodecheck/GitHubRepositorySearchActivity.kt
@@ -3,9 +3,9 @@ package com.example.androidengineercodecheck
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class GitHubRepositorySearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.github_repository_search_activity)
     }
 }
