a263257043d6ef02574c25709999037950bc39dc
shusuke0812 <shu.ota0812@gmail.com>
2021-01-12 23:32:18 +0900

    [add]GitHubRepositoryのデータクラスを作成

--- app/src/main/java/com/example/androidengineercodecheck/Entity/GitHubRepository.kt
+++ app/src/main/java/com/example/androidengineercodecheck/Entity/GitHubRepository.kt
@@ -1,4 +1,34 @@
 package com.example.androidengineercodecheck.Entity
 
-data class GitHubRepository {
-}
+import com.squareup.moshi.Json
+
+data class GitHubRepository (
+    var items: List<Item>
+)
+data class Item (
+    // リポジトリ名
+    @Json(name = "full_name")
+    var name: String,
+    // リポジトリ言語
+    @Json(name = "language")
+    var language: String,
+    // スター数
+    @Json(name = "stargazers_count")
+    var starNumber: Int,
+    // ウォッチ数
+    @Json(name = "watchers_count")
+    var watchNumber: Int,
+    // フォーク数
+    @Json(name = "forks_count")
+    var forkNumber: Int,
+    // イシュー数
+    @Json(name = "open_issues_ccount")
+    var isueNumber: Int,
+    // オーナー情報
+    var owner: Owner
+)
+data class Owner (
+    // アバター画像URL
+    @Json(name = "avatar_url")
+    var avatarImage: String
+)
