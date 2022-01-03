f03b216f3432f5e21aeb27b64aa06bc3f045a123
shusuke0812 <shu.ota0812@gmail.com>
2022-01-03 12:47:42 +0900

    fix: OneFragmentの名称、インデント、スペース等を修正

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchRepositoryFragment.kt
@@ -0,0 +1,94 @@
+/*
+ * Copyright © 2021 YUMEMI Inc. All rights reserved.
+ */
+package jp.co.yumemi.android.code_check
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.*
+import jp.co.yumemi.android.code_check.databinding.FragmentSearchRepositoryBinding
+
+/**
+ * GitHubのリポジトリ検索結果を一覧表示する Fragment
+ */
+class SearchRepositoryFragment: Fragment(R.layout.fragment_search_repository) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentSearchRepositoryBinding.bind(view)
+        val viewModel = OneViewModel(context!!)
+
+        val layoutManager= LinearLayoutManager(context!!)
+        val dividerItemDecoration = DividerItemDecoration(context!!, layoutManager.orientation)
+
+        val adapter = CustomAdapter(object: CustomAdapter.OnItemClickListener {
+            override fun itemClick(item: item) {
+                gotoRepositoryFragment(item)
+            }
+        })
+
+        binding.searchInputText.setOnEditorActionListener { editText, action, _ ->
+            if (action == EditorInfo.IME_ACTION_SEARCH) {
+                editText.text.toString().let {
+                        viewModel.searchResults(it).apply {
+                            adapter.submitList(this)
+                        }
+                }
+                return@setOnEditorActionListener true
+            }
+            return@setOnEditorActionListener false
+        }
+
+        binding.recyclerView.also {
+            it.layoutManager = layoutManager
+            it.addItemDecoration(dividerItemDecoration)
+            it.adapter = adapter
+        }
+    }
+
+    fun gotoRepositoryFragment(item: item) {
+        val action = SearchRepositoryFragmentDirections.actionRepositoriesFragmentToRepositoryFragment(item = item)
+        findNavController().navigate(action)
+    }
+}
+
+val diffUtil = object: DiffUtil.ItemCallback<item>() {
+
+    override fun areItemsTheSame(oldItem: item, newItem: item): Boolean {
+        return oldItem.name == newItem.name
+    }
+
+    override fun areContentsTheSame(oldItem: item, newItem: item): Boolean {
+        return oldItem == newItem
+    }
+}
+
+class CustomAdapter(private val itemClickListener: OnItemClickListener, ) : ListAdapter<item, CustomAdapter.ViewHolder>(diffUtil) {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+    interface OnItemClickListener {
+    	fun itemClick(item: item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : ViewHolder {
+    	val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_item, parent, false)
+    	return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    	val item = getItem(position)
+        (holder.itemView.findViewById<View>(R.id.repositoryNameView) as TextView).text = item.name
+
+    	holder.itemView.setOnClickListener {
+     		itemClickListener.itemClick(item)
+    	}
+    }
+}
