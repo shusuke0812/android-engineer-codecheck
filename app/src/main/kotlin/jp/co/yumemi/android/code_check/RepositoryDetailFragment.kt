d197e37f3ff48ca66e4334bf252c9424f73dff10
shusuke0812 <shu.ota0812@gmail.com>
2022-01-03 16:02:04 +0900

    fix: TwoFragmentの名称、インデント、スペース等を修正

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoryDetailFragment.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright © 2021 YUMEMI Inc. All rights reserved.
+ */
+package jp.co.yumemi.android.code_check
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import coil.load
+import jp.co.yumemi.android.code_check.TopActivity.Companion.lastSearchDate
+import jp.co.yumemi.android.code_check.databinding.FragmentRepositoryDetailBinding
+
+class RepositoryDetailFragment : Fragment(R.layout.fragment_repository_detail) {
+
+    private val args: RepositoryDetailFragmentArgs by navArgs()
+
+    private var binding: FragmentRepositoryDetailBinding? = null
+    private val _binding get() = binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Log.d("検索した日時", lastSearchDate.toString())
+
+        binding = FragmentRepositoryDetailBinding.bind(view)
+
+        var repository = args.repository
+
+        _binding.ownerIconView.load(repository.ownerIconUrl)
+        _binding.nameView.text = repository.name;
+        _binding.languageView.text = repository.language;
+        _binding.starsView.text = "${repository.stargazersCount} stars";
+        _binding.watchersView.text = "${repository.watchersCount} watchers";
+        _binding.forksView.text = "${repository.forksCount} forks";
+        _binding.openIssuesView.text = "${repository.openIssuesCount} open issues";
+    }
+}
