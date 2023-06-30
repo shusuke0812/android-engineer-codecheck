12b79c95b3186d41de2500c6b633b579598da345
shusuke0812 <shu.ota0812@gmail.com>
2023-06-30 23:30:48 +0900

    add: GitHubSearchRepositoryクラスを追加

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/GitHubSearchRepository.kt
@@ -0,0 +1,36 @@
+package jp.co.yumemi.android.code_check.repository
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import jp.co.yumemi.android.code_check.model.Repository
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface GitHubRepositoryInterface {
+    @Headers("Accept", "application/vnd.github.v3+json")
+    @GET("search/repositories")
+    suspend fun getRepositories(
+        @Query("q") q: String
+    ) : Response<Repository>
+}
+
+class GitHubSearchRepository {
+    private val _api: GitHubRepositoryInterface by lazy {
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+        retrofit.create(GitHubRepositoryInterface::class.java)
+    }
+
+    suspend fun getRepositories(inputText: String): Response<Repository> {
+        return _api.getRepositories(q = inputText)
+    }
+}
