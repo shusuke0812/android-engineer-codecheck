cfa970150d6e87c484b73156e6d59ff185f3f00f
shusuke0812 <shu.ota0812@gmail.com>
2023-06-30 22:09:01 +0900

    add: レスポンス型にmoshiを導入し、JSONをシリアライズできるようにした

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/model/Repository.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/model/Repository.kt
@@ -1,15 +1,34 @@
 package jp.co.yumemi.android.code_check.model
 
 import android.os.Parcelable
+import com.squareup.moshi.Json
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Repository(
-    val name: String,
-    val ownerIconUrl: String,
+    @Json(name = "full_name")
+    val fullName: String,
+
+    @Json(name = "owner")
+    val owner: Owner,
+
+    @Json(name = "language")
     val language: String,
+
+    @Json(name = "stargazers_count")
     val stargazersCount: Long,
+
+    @Json(name = "watchers_count")
     val watchersCount: Long,
+
+    @Json(name = "forks_count")
     val forksCount: Long,
+
+    @Json(name = "open_issues_count")
     val openIssuesCount: Long,
+) : Parcelable
+@Parcelize
+data class Owner(
+    @Json(name = "avatar_url")
+    val avatarUrl: String
 ) : Parcelable
