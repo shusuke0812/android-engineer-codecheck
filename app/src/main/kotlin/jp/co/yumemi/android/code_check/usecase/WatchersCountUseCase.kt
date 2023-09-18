3ee450b46dd95f2047e6e52a123106fc7c3c6b79
shusuke0812 <shu.ota0812@gmail.com>
2023-09-18 22:19:27 +0900

    add: watchers_countを取得するUseCaseクラスを追加

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/usecase/WatchersCountUseCase.kt
@@ -0,0 +1,16 @@
+package jp.co.yumemi.android.code_check.usecase
+
+import jp.co.yumemi.android.code_check.repository.GitHubGetRepository
+
+class WatchersCountUseCase(
+    private val gitHubGetRepository: GitHubGetRepository = GitHubGetRepository()
+) {
+    suspend operator fun invoke(fullName: String): Int? {
+        val response = gitHubGetRepository.getRepository(fullName = fullName)
+        return if (response.isSuccessful) {
+            response.body()?.subscribersCount
+        } else {
+            null
+        }
+    }
+}
