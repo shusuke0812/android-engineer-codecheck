05f87ecc24e461130a4b7c6cc34f7a3961da1316
shusuke0812 <shu.ota0812@gmail.com>
2023-09-24 17:53:26 +0900

    fix: GetWatchersCountUseCaseのinvoke引数を修正

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/usecase/GetWatchersCountUseCase.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/usecase/GetWatchersCountUseCase.kt
@@ -1,13 +1,12 @@
 package jp.co.yumemi.android.code_check.usecase
 
-import android.util.Log
 import jp.co.yumemi.android.code_check.repository.GitHubGetRepository
 
-class WatchersCountUseCase(
+class GetWatchersCountUseCase(
     private val gitHubGetRepository: GitHubGetRepository = GitHubGetRepository()
 ) {
-    suspend operator fun invoke(fullName: String): Int? {
-        val response = gitHubGetRepository.getRepository(fullName = fullName)
+    suspend operator fun invoke(ownerName: String, reposName: String): Int? {
+        val response = gitHubGetRepository.getRepository(ownerName, reposName)
         return if (response.isSuccessful) {
             response.body()?.subscribersCount
         } else {
