89b89ba6218a2f77e43897220a9f27663428fb7a
shusuke0812 <shu.ota0812@gmail.com>
2023-09-24 17:57:46 +0900

    fix: WatchersCount表示をGet repository APIを使って更新するようにした

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/screen/detail/RepositoryDetailFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/screen/detail/RepositoryDetailFragment.kt
@@ -7,11 +7,13 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import coil.load
 import jp.co.yumemi.android.code_check.R
 import jp.co.yumemi.android.code_check.databinding.FragmentRepositoryDetailBinding
-import jp.co.yumemi.android.code_check.screen.search.SearchRepositoryViewModel
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
 
 /**
  * GitHubのリポジトリ詳細を表示する Fragment
@@ -20,6 +22,7 @@ class RepositoryDetailFragment : Fragment(R.layout.fragment_repository_detail) {
 
     private val args: RepositoryDetailFragmentArgs by navArgs()
     private lateinit var binding: FragmentRepositoryDetailBinding
+    private val viewModel: RepositoryDetailViewModel by inject()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -35,5 +38,14 @@ class RepositoryDetailFragment : Fragment(R.layout.fragment_repository_detail) {
         binding.watchersView.text = "${repository.watchersCount} watchers";
         binding.forksView.text = "${repository.forksCount} forks";
         binding.openIssuesView.text = "${repository.openIssuesCount} open issues";
+
+        viewModel.getWatchersCount(repository.owner?.login ?: "", repository.name)
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.uiState.collect { uiState ->
+                if (!uiState.isLoading && uiState.watchersCount != null) {
+                    binding.watchersView.text = "${uiState.watchersCount} watchers"
+                }
+            }
+        }
     }
 }
