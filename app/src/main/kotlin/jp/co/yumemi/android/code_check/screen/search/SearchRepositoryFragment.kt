f61af8a87bdad2ebc1cd65b3f14cf5f563becb41
shusuke0812 <shu.ota0812@gmail.com>
2023-09-24 21:56:40 +0900

    fix: SearchRepositoryFragmentからRecyclerView.Adapterを別ファイルに切り出した

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/screen/search/SearchRepositoryFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/screen/search/SearchRepositoryFragment.kt
@@ -5,29 +5,25 @@ package jp.co.yumemi.android.code_check.screen.search
 
 import android.os.Bundle
 import android.view.KeyEvent
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.*
 import jp.co.yumemi.android.code_check.R
 import jp.co.yumemi.android.code_check.databinding.FragmentSearchRepositoryBinding
 import jp.co.yumemi.android.code_check.model.Repository
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
 
 /**
  * GitHubのリポジトリ検索結果を一覧表示する Fragment
  */
 class SearchRepositoryFragment : Fragment(R.layout.fragment_search_repository) {
 
-    private val viewModel: SearchRepositoryViewModel by viewModels()
-    private lateinit var adapter: CustomAdapter
+    private val viewModel: SearchRepositoryViewModel by inject()
+    private lateinit var adapter: SearchRepositoryItemAdapter
     private lateinit var binding: FragmentSearchRepositoryBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -38,7 +34,7 @@ class SearchRepositoryFragment : Fragment(R.layout.fragment_search_repository) {
         val layoutManager= LinearLayoutManager(requireContext())
         val dividerItemDecoration = DividerItemDecoration(requireContext(), layoutManager.orientation)
 
-        adapter = CustomAdapter(object: CustomAdapter.OnItemClickListener {
+        adapter = SearchRepositoryItemAdapter(object: SearchRepositoryItemAdapter.OnItemClickListener {
             override fun itemClick(repository: Repository) {
                 gotoRepositoryDetailFragment(repository)
             }
@@ -72,41 +68,5 @@ class SearchRepositoryFragment : Fragment(R.layout.fragment_search_repository) {
     private fun gotoRepositoryDetailFragment(repository: Repository) {
         val action = SearchRepositoryFragmentDirections.actionRepositoriesFragmentToRepositoryFragment(repository = repository)
         findNavController().navigate(action)
-    }
-}
-
-// TODO: この行以降に記載されたRecyclerViewに関する設定コードは別ファイルに切り出したい( search/adapterディレクトリ配下 )
-val diffUtil = object: DiffUtil.ItemCallback<Repository>() {
-
-    override fun areItemsTheSame(oldRepository: Repository, newRepository: Repository): Boolean {
-        return oldRepository.fullName == newRepository.fullName
-    }
-
-    override fun areContentsTheSame(oldRepository: Repository, newRepository: Repository): Boolean {
-        return oldRepository == newRepository
-    }
-}
-
-class CustomAdapter(private val itemClickListener: OnItemClickListener, ) : ListAdapter<Repository, CustomAdapter.ViewHolder>(diffUtil) {
-
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
-
-    interface OnItemClickListener {
-    	fun itemClick(repository: Repository)
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : ViewHolder {
-    	val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_item, parent, false)
-    	return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-    	val item = getItem(position)
-        val textView = holder.itemView.findViewById<TextView>(R.id.repositoryNameView)
-        textView.text = item.fullName
-
-    	holder.itemView.setOnClickListener {
-     		itemClickListener.itemClick(item)
-    	}
     }
 }
---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/screen/search/SearchRepositoryItemAdapter.kt
@@ -0,0 +1,46 @@
+package jp.co.yumemi.android.code_check.screen.search
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.*
+import jp.co.yumemi.android.code_check.R
+import jp.co.yumemi.android.code_check.model.Repository
+
+class SearchRepositoryItemAdapter(
+    private val itemClickListener: OnItemClickListener
+) : ListAdapter<Repository, SearchRepositoryItemAdapter.ViewHolder>(diffUtil) {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+    interface OnItemClickListener {
+        fun itemClick(repository: Repository)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        val textView = holder.itemView.findViewById<TextView>(R.id.repositoryNameView)
+        textView.text = item.fullName
+
+        holder.itemView.setOnClickListener {
+            itemClickListener.itemClick(item)
+        }
+    }
+}
+
+private val diffUtil = object: DiffUtil.ItemCallback<Repository>() {
+
+    override fun areItemsTheSame(oldRepository: Repository, newRepository: Repository): Boolean {
+        return oldRepository.fullName == newRepository.fullName
+    }
+
+    override fun areContentsTheSame(oldRepository: Repository, newRepository: Repository): Boolean {
+        return oldRepository == newRepository
+    }
+}
