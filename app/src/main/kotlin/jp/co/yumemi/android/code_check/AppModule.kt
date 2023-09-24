5235b359c56aae0789b07b77de974225ebe955e6
shusuke0812 <shu.ota0812@gmail.com>
2023-09-24 17:37:55 +0900

    fix: AppModule

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/AppModule.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/AppModule.kt
@@ -3,9 +3,14 @@ package jp.co.yumemi.android.code_check
 import jp.co.yumemi.android.code_check.screen.detail.RepositoryDetailViewModel
 import jp.co.yumemi.android.code_check.usecase.GetWatchersCountUseCase
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.Module
 import org.koin.dsl.module
 
-val appModules = module {
+val appModules: Module get() = module {
+    includes(repositoryDetailModules)
+}
+
+private val repositoryDetailModules = module {
     factory { GetWatchersCountUseCase() }
     viewModel { RepositoryDetailViewModel( get() ) }
 }
