cf88f04bfe734c9f72d5f72c3321bddd0af98a28
shusuke0812 <shu.ota0812@gmail.com>
2023-09-24 21:54:46 +0900

    fix: SearchRepositoryFragment/VMにkoinを適用

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/AppModule.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/AppModule.kt
@@ -1,13 +1,21 @@
 package jp.co.yumemi.android.code_check
 
 import jp.co.yumemi.android.code_check.screen.detail.RepositoryDetailViewModel
+import jp.co.yumemi.android.code_check.screen.search.SearchRepositoryViewModel
 import jp.co.yumemi.android.code_check.usecase.GetWatchersCountUseCase
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val appModules: Module get() = module {
-    includes(repositoryDetailModules)
+    includes(
+        repositoryDetailModules,
+        searchRepositoryModules
+    )
+}
+
+private val searchRepositoryModules = module {
+    viewModel { SearchRepositoryViewModel() }
 }
 
 private val repositoryDetailModules = module {
