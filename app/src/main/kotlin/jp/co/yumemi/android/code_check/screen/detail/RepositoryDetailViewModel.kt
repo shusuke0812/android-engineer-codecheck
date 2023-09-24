058d73a7f2222eb4a12e39567ae69606910e60e3
shusuke0812 <shu.ota0812@gmail.com>
2023-09-24 17:57:00 +0900

    fix: 微修正

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/screen/detail/RepositoryDetailViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/screen/detail/RepositoryDetailViewModel.kt
@@ -2,31 +2,30 @@ package jp.co.yumemi.android.code_check.screen.detail
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import jp.co.yumemi.android.code_check.usecase.WatchersCountUseCase
+import jp.co.yumemi.android.code_check.usecase.GetWatchersCountUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class RepositoryDetailViewModel(
-    private val watchersCountUseCase: WatchersCountUseCase,
-    private val fullName: String
+    private val getWatchersCountUseCase: GetWatchersCountUseCase
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(RepositoryDetailUiState())
     val uiState: StateFlow<RepositoryDetailUiState> = _uiState
 
-    init {
+    fun getWatchersCount(ownerName: String, reposName: String) {
         viewModelScope.launch {
-            val watchersCount = watchersCountUseCase(fullName = fullName)
+            val watchersCount = getWatchersCountUseCase(ownerName, reposName)
             _uiState.update { currentUiState ->
-                currentUiState.copy(watchersCount = watchersCount)
+                currentUiState.copy(watchersCount = watchersCount, isLoading = false)
             }
         }
     }
 }
 
 data class RepositoryDetailUiState(
-    val isLoading: Boolean = false,
+    val isLoading: Boolean = true,
     val watchersCount: Int? = null,
     val errorMessage: String? = null
 )
