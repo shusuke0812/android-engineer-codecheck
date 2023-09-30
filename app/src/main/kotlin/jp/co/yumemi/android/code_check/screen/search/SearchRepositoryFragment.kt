083d9ed4167a493fe5f89b182ca54560bfe92994
shusuke0812 <shu.ota0812@gmail.com>
2023-09-30 19:54:03 +0900

    fix: keyboard closed when search repositories

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/screen/search/SearchRepositoryFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/screen/search/SearchRepositoryFragment.kt
@@ -3,10 +3,12 @@
  */
 package jp.co.yumemi.android.code_check.screen.search
 
+import android.content.Context
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -45,6 +47,7 @@ class SearchRepositoryFragment : Fragment(R.layout.fragment_search_repository) {
                 editText.text.toString().let {
                     viewModel.searchRepositories(inputText = it)
                 }
+                closeKeyboard()
                 return@setOnEditorActionListener true
             }
             return@setOnEditorActionListener false
@@ -68,5 +71,10 @@ class SearchRepositoryFragment : Fragment(R.layout.fragment_search_repository) {
     private fun gotoRepositoryDetailFragment(repository: Repository) {
         val action = SearchRepositoryFragmentDirections.actionRepositoriesFragmentToRepositoryFragment(repository = repository)
         findNavController().navigate(action)
+    }
+
+    private fun closeKeyboard() {
+        val system = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        system?.hideSoftInputFromWindow(view?.windowToken, 0)
     }
 }
